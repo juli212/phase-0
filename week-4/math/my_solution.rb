df94831cfac83cda72c2b28cd7f70190f589f51a
juli212 <jjgiraldi@gmail.com>
2016-02-09 20:28:07 -0500

    Add 4.3.2 solution

---
+++ week-4/math/my_solution.rb
@@ -0,0 +1,26 @@
+# Math Methods
+
+# I worked on this challenge [by myself ].
+
+
+# Your Solution Below
+
+def add(num_1, num_2)
+	num_1 + num_2
+ #your code here
+end
+
+def subtract(num_1, num_2)
+	num_1 - num_2
+ #your code here
+end
+
+def multiply(num_1, num_2)
+	num_1 * num_2
+ #your code here
+end
+
+def divide(num_1, num_2)
+	num_1.to_f / num_2.to_f
+ #your code here
+end
--- week-4/variables-methods.rb
+++ week-4/variables-methods.rb
@@ -9,3 +9,26 @@ puts
 puts 'What is your favorite number?'
 fav_num = gets.chomp.to_i + 1 
 puts 'What a silly number!! How about a bigger, better number like ' + fav_num.to_s + ' instead?'
+
+=begin
+	
+How do you define a local variable?
+	variable_name = variable
+
+How do you define a method?
+	def method_name(parameter1, parameter2)
+
+What is the difference between a local variable and a method?
+	A variable is an object while a method is something that does an action to the variable/
+
+How do you run a ruby program from the command line?
+	ruby program_name.rb
+
+How do you run an RSpec file from the command line?
+	rspec rspec_file_name.rb
+
+What was confusing about this material? What made sense?
+	I kept overthinking my code and adding too much. Everything was so simple that when I ran the code with rspec and got no errors, I always thought my code was missing someething until I tested it. The most difficult thing for me was interpreting error messages and finding solutions (which usually just involved deleting a piece of my code).	
+URL for 4.3.1:
+URL for 4.3.2: 
+=end
