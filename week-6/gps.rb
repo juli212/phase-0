1440adce4c6a7ff0ab14806891dd22c5c550af50
julia <jjgiraldi@gmail.com>
2016-02-24 12:56:17 -0500

    add solution to gps 2.3

--- week-6/gps.rb
+++ week-6/gps.rb
@@ -1,53 +1,63 @@
 # Your Names
-# 1)
-# 2)
+# 1) Wesley El-Amin
+# 2) Julia Giraldi
 
-# We spent [#] hours on this challenge.
+# We spent [2] hours on this challenge.
 
 # Bakery Serving Size portion calculator.
 
 def serving_size_calc(item_to_make, num_of_ingredients)
   library = {"cookie" => 1, "cake" =>  5, "pie" => 7}
-  error_counter = 3
+  
 
-  library.each do |food|
-    if library[food] != library[item_to_make]
-      error_counter += -1
-    end
+  
+  # defining a method that takes 2 arguments and compares them to an existing hash library
+# error counter = 3
+  
+  unless library.has_key?(item_to_make)
+    raise ArgumentError.new("{#item_to_make} is not a valid input")
   end
 
-  if error_counter > 0
-    raise ArgumentError.new("#{item_to_make} is not a valid input")
-  end
-
-  serving_size = library.values_at(item_to_make)[0]
+  serving_size = library[item_to_make]
   remaining_ingredients = num_of_ingredients % serving_size
+  suggested_portion = num_of_ingredients / serving_size
+  suggested_item = []
+  if remaining_ingredients > 0
+    library.each { |key, value| suggested_item.push(key) if value <= remaining_ingredients}
+  end
 
-  case remaining_ingredients
-  when 0
-    return "Calculations complete: Make #{num_of_ingredients / serving_size} of #{item_to_make}"
+  
+  #key(remaining_ingredients)
+# when the remaining ingredients are equal to zero, the method returns the amount to make.
+    
+# when the remaining ingredients is not equal to zero, the method returns the amount to make and the leftover ingredients. Also, there should be a feature that tells you what to make with the leftover ingredients.
+  
+  
+  if remaining_ingredients == 0
+    return "Calculations complete: Make #{suggested_portion} of #{item_to_make}"
   else
-    return "Calculations complete: Make #{num_of_ingredients / serving_size} of #{item_to_make}, you have #{remaining_ingredients} leftover ingredients. Suggested baking items: TODO: MAKE THIS FEATURE"
+    return "Calculations complete: Make #{suggested_portion} of #{item_to_make}, you have #{remaining_ingredients} leftover ingredients. Suggested baking items:  #{suggested_item.join(", ")}" 
   end
 end
 
+
+
+
+
 p serving_size_calc("pie", 7)
-p serving_size_calc("pie", 8)
+p serving_size_calc("pie", 83)
 p serving_size_calc("cake", 5)
 p serving_size_calc("cake", 7)
 p serving_size_calc("cookie", 1)
 p serving_size_calc("cookie", 10)
-p serving_size_calc("THIS IS AN ERROR", 5)
+#p serving_size_calc("pizza", 5)
+
+
 
 #  Reflection
 =begin
 What did you learn about making code readable by working on this challenge?
-
 Did you learn any new methods? What did you learn about them?
-
 What did you learn about accessing data in hashes? 
-
 What concepts were solidified when working through this challenge?
-
 =end
-
