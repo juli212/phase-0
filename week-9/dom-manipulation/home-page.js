25e9fa3ca6a0c9d0a03830eb3fb64c0c8b650fbe
julia <jjgiraldi@gmail.com>
2016-03-15 13:00:10 -0400

    add 9.3 files, not finished

---
+++ week-9/dom-manipulation/home-page.js
@@ -0,0 +1,72 @@
+// DOM Manipulation Challenge
+
+
+// I worked on this challenge [by myself, with: ].
+
+
+// Add your JavaScript calls to this page:
+
+// Release 0:
+
+// Release 1:
+
+
+document.GetElementById("release-0").className = "done";
+
+// Release 2:
+
+  document.getElementById("release-1").style.display="none";
+
+// Release 3:
+
+  document.getElementsByTagName("h1")[0].innerHTML="I completed release 2.";
+ // document.getElementById("h1id").innerHTML = "I completed release 2.";
+
+// Release 4:
+
+document.getElementById("release-3").style.backgroundColor = "#955251";
+
+
+// Release 5:
+
+document.getElementsByClassName("release-4").style.fontSize = "2em";
+
+// Release 6:
+
+var tmpl = document.getElementById("hidden");
+document.body.appendChild(tmpl.content.cloneNode(true));
+
+// DOM Manipulation Challenge
+
+
+// I worked on this challenge [by myself, with: ].
+
+
+// Add your JavaScript calls to this page:
+
+// Release 0:
+
+
+
+
+// Release 1:
+
+
+
+
+// Release 2:
+
+
+
+
+// Release 3:
+
+
+
+
+// Release 4:
+
+
+
+
+// Release 5:
